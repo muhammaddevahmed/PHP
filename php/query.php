d233e152d7409ce1567a5a7c9a26f4434bcfb38c
Muhammad Ahmed <m.ahmed.uh72@gmail.com>
2025-01-17 01:00:01 +0500

    register details to database

--- php/query.php
+++ php/query.php
@@ -16,11 +16,35 @@ if(isset($_POST['addUser'])){
     if(empty($userEmail)){
         $userEmailErr = "Email is required";
     }
-    if(empty($userPassword)){
+    else{
+        $query = $pdo ->prepare("select * from users where email = :userEmail");
+        $query->bindParam('userEmail',$userEmail);
+        $query->execute();
+        $user = $query->fetch(PDO::FETCH_ASSOC);
+        if($user){
+            $userEmailErr = 'email already exist';
+        }
+       
+    }
+    if (empty($userPassword)) {
         $userPasswordErr = "Password is required";
     }
-    if(empty($userConfirmPassword)){
+    
+    if (empty($userConfirmPassword)) {
         $userConfirmPasswordErr = "Confirm Your Password";
+    } elseif ($userPassword !== $userConfirmPassword) {
+        $userConfirmPasswordErr = "Passwords do not match";
+    }
+    
+   
+    if (empty($userNameErr) && empty($userEmailErr) && empty($userPasswordErr) && empty($userConfirmPasswordErr) ){
+        $query = $pdo->prepare("insert into users(name,email,password) values(:name , :email, :password)");
+        $query->bindParam('name',$userName);
+        $query->bindParam('email',$userEmail);
+        $query->bindParam('password',$userPassword);
+        $query->execute();
+        echo "<script>alert('register succesfully');location.assign('register.php')</script>";
+
     }
 }
 ?>
